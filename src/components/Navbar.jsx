44e79f55cbfe136801599ea29ee3d571b3b064e3
Shawaiz Khan <shawaizkhan.biz@gmail.com>
2024-08-29 11:09:56 +0100

    feat: hero section layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ function classNames(...classes) {
 
 export default function Navbar() {
     return (
-        <Disclosure as="nav" className="bg-sky-900">
+        <Disclosure as="nav" className="bg-transparent">
             <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                 <div className="relative flex h-16 items-center justify-between">
                     <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
@@ -37,7 +37,7 @@ export default function Navbar() {
                                         key={item.name}
                                         href={item.href}
                                         aria-current={item.current ? 'page' : undefined}
-                                        className={classNames(item.current ? 'bg-sky-950 text-white' : 'text-gray-200 hover:bg-sky-800 hover:text-white', 'rounded-md px-3 py-2 text-sm font-medium',)}>
+                                        className={classNames(item.current ? 'bg-blue-950 text-white' : 'text-gray-200 hover:bg-blue-900 hover:text-white transition-all duration-300 ease-in', 'rounded-md px-3 py-2 text-sm font-medium',)}>
                                         {item.name}
                                     </a>
                                 ))}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,27 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import Navbar from '../components/Navbar';
+import backgroundImage from '../assets/images/heroImg.jpg';
 
 export default function Home() {
     return (
         <main>
-            <Navbar />
+            {/* Hero Section  */}
+            <section
+                className="relative h-[35rem] bg-center bg-cover bg-fixed flex flex-col justify-between"
+                style={{ backgroundImage: `url(${backgroundImage})` }}>
+                <div className="absolute inset-0 bg-black opacity-50"></div>
+                {/* Navigation Bar */}
+                <nav className="relative z-10 px-5 pt-5">
+                    <Navbar />
+                </nav>
+                {/* Main Content */}
+                <div className="relative z-10 flex-grow flex flex-col justify-center pl-16">
+                    <h1 className="text-5xl font-bold text-white mb-4">BrightCast</h1>
+                    <p className="text-lg text-white max-w-md">Discover real-time weather updates, detailed forecasts, and
+                        insights tailored to your location. Stay prepared for any weather, day or night, with BrightCast.</p>
+                </div>
+            </section>
         </main>
     )
 }
